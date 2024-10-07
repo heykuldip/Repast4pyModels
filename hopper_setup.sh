8863ab72d7946a7f05d401cc0367970697cd3379
Rory <mrrgvm@gmail.com>
2024-10-07 11:29:30 -0400

    fixing env setup

--- hopper_setup.sh
+++ hopper_setup.sh
@@ -3,17 +3,23 @@
 # It's a bit of a chicken/egg situation since you have to load git first
 # in order to get this script in order to load git...
 
+echo "============================="
+echo "== Setting up Slurm env... =="
+echo "============================="
+
 echo "Loading modules..."
 module load gnu10
 module load openmpi
 module load git
 
 echo "Creating conda environment..."
-conda create --name repast4py_test python=3.9 pip
+source miniconda/bin/activate 
+conda create --name repast4py_env python=3.9 pip
+conda activate repast4py_env
 env CC=mpicxx CXX=mpicxx pip install repast4py typing-extensions packaging snuggs pandas
 
-echo "Cloning project from git..."
-git clone https://github.com/heykuldip/Repast4pyModels.git
+# echo "Cloning project from git..."
+# git clone https://github.com/heykuldip/Repast4pyModels.git
 
 echo "Setting up Slurm..."
 salloc -p normal -q normal -n 1 --ntasks-per-node=24 --mem=50GB
